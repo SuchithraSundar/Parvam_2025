303a89c04748fd1057dabeaf8976ec3d1fca5b4f
SuchithraSundar <suchithras.parvam@gmail.com>
2025-04-08 16:23:42 +0530

    first commit

--- springboot_framework/SpringBootCRUD/src/main/java/com/geccrud/SpringBootCRUD/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geccrud.SpringBootCRUD.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.geccrud.SpringBootCRUD.models.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-
-}
