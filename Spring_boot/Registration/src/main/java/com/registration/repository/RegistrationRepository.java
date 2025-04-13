56ef05e82a24f64e30d9a3332115c8a7596ed862
SuchithraSundar <suchithras.parvam@gmail.com>
2025-04-13 18:40:29 +0530

    first commit

--- Spring_boot/Registration/src/main/java/com/registration/repository/RegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.registration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.registration.model.Registration;
-
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {
-}
